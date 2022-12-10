90a85b231e902345a1b8ed7ea22d6e873895b32c
DevIdol <devidol.mm@gmail.com>
2022-12-11 02:15:09 +0630

    Add Word-Replace

---
+++ sec02/word-replace.py
@@ -0,0 +1,6 @@
+sentence = input("Enter your sentence: ")
+print('Your sentence is:', sentence);
+word1 = input("Enter the word to replace: ")
+word2 = input("Enter the word to replace it width: ")
+
+print(sentence.replace(word1, word2))
