8ee296709ae64fb9a4a0d691fd7610785d4cd565
DevIdol <devidol.mm@gmail.com>
2022-12-11 01:14:39 +0630

    Add Variables

---
+++ sec01/variables.py
@@ -0,0 +1,6 @@
+name = "Joe"
+age = 20
+
+print("My name is " + name); # "+" is can use only string type
+print("I'm ", age , "years old.") # "," can use both string and int
+print("My name is", name,". I am ", age, " years old. ")
