34b846de70f7f8c51ea7cd2670479b9e633883db
DevIdol <devidol.mm@gmail.com>
2022-12-11 02:15:41 +0630

    Add List

---
+++ sec02/list.py
@@ -0,0 +1,8 @@
+countries = ["Myanmar", "Thailand", "Myalaysia", "Singapore"]
+
+print(countries)
+print(countries[1])
+print(countries[1][3])
+print(countries[3:])
+print(countries[2:3])
+print(countries[1:3])
