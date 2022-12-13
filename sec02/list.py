50dcdaf8b346a95ff9ee6463e604ccb4c351ce8b
DevIdol <devidol.mm@gmail.com>
2022-12-13 15:22:47 +0630

    Modified list

--- sec02/list.py
+++ sec02/list.py
@@ -1,8 +1,15 @@
-countries = ["Myanmar", "Thailand", "Myalaysia", "Singapore"]
+countries = ["Myanmar", "Thailand", "Myalaysia", "Singapore", 2, True]
 
-print(countries)
-print(countries[1])
-print(countries[1][3])
-print(countries[3:])
-print(countries[2:3])
-print(countries[1:3])
+print(countries) #['Myanmar', 'Thailand', 'Myalaysia', 'Singapore', 2, True]
+print(countries[1]) #Thailand
+print(countries[-2]) #Myalaysia
+print(countries[1][2]) #[1] => Thailand / [2] => a
+print(countries[3:]) #["Singapore"]
+print(countries[2:3]) #["Myalaysia"]
+print(countries[1:3]) #["Thailand", "Myalaysia"]
+print(len(countries)) #4
+print(type(countries)) #<class 'list'>
+print(type(countries[5]))#<class 'bool'>
+
+countries2 = list(("Myanmar", "Thailand", "Myalaysia", "Singapore", 2, True))
+print(countries2)
