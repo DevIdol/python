db9f9f810c32849867b069cd3a5a0fd8f4624d40
DevIdol <devidol.mm@gmail.com>
2022-12-13 21:28:40 +0630

    Add Arithmetic

---
+++ sec01/arithmetic.py
@@ -0,0 +1,10 @@
+a= 10
+b = 5
+
+print("Sum:", a + b) #Sum: 15
+print("Subtract:", a - b) #Subtract: 5
+print("Mult:", a * b) #Mult: 50
+print("Float Division:", a / b) #Float Division: 2.0
+print("Integer Division:", a // b) #Integer Division: 2
+print("Power:", a ** b) #Power: 100000
+print("Remainder:", a % b); #Remainder: 0
