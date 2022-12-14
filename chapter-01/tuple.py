1eda41e37bfd9bcb09d4fa6771a8678666e92598
DevIdol <devidol.mm@gmail.com>
2022-12-14 23:54:38 +0630

    Add Tuple

--- chapter-01/tuple.py
+++ chapter-01/tuple.py
@@ -1,9 +1,9 @@
-#Tuple is read only
+# Tuple is read only
 
-ages = (18, 20);
+ages = (18, 20)
 
-print("Ages =>", ages, type(ages)) #Ages => (18, 20) <class 'tuple'>
-print("Ages[1] =>", ages[1]) #Ages[1] => 20
+print("Ages =>", ages, type(ages))  # Ages => (18, 20) <class 'tuple'>
+print("Ages[1] =>", ages[1])  # Ages[1] => 20
 
 # ages[0] = 18;
 # print(ages) #TypeError: 'tuple' object does not support item assignment
