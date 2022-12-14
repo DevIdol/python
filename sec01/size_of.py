238473e0a5be011cd17ffb8e8d62cb48b3840ba4
DevIdol <devidol.mm@gmail.com>
2022-12-14 22:28:47 +0630

    Add Sizeof

---
+++ sec01/size_of.py
@@ -0,0 +1,7 @@
+import sys
+
+x = 10
+print("get size of number =>", sys.getsizeof(x)) #get size of number => 28
+
+x = 10.4
+print("get size of float =>", sys.getsizeof(x)) #get size of float => 24
