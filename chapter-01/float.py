e40bfb43a949de256c64981a4ccd16f7bbd92464
DevIdol <devidol.mm@gmail.com>
2022-12-14 23:47:29 +0630

    Add Float

--- chapter-01/float.py
+++ chapter-01/float.py
@@ -1,7 +1,7 @@
 my_float = 20.
-print("my float=> ", my_float) #my float=>  20.0
+print("my float=> ", my_float)  # my float=>  20.0
 
-my_float = 1.1e2 # 1.1 * 10 ** 2
-print("my float=> ", my_float) #my float=>  110.0
+my_float = 1.1e2  # 1.1 * 10 ** 2
+print("my float=> ", my_float)  # my float=>  110.0
 
-print("0.3 - 0.2 => ", 0.3 - 0.2) #0.3 - 0.2 =>  0.09999999999999998
+print("0.3 - 0.2 => ", 0.3 - 0.2)  # 0.3 - 0.2 =>  0.09999999999999998
