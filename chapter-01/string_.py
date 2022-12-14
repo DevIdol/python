f36d0aa4efb819bc68f31bf96a1921582829361a
DevIdol <devidol.mm@gmail.com>
2022-12-14 23:47:03 +0630

    Add String

--- chapter-01/string_.py
+++ chapter-01/string_.py
@@ -6,6 +6,6 @@ print('my_str => ', my_str)  # my_str =>  Hello 'Myanmar'
 
 my_str = """Hello 
 'Myanmar'"""
-print('my_str => ', my_str)  
+print('my_str => ', my_str)
 # my_str =>  Hello
 # 'Myanmar'
