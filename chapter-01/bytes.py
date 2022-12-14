1fc5cb9ffec477e75644b2d0022020ec80b42a8f
DevIdol <devidol.mm@gmail.com>
2022-12-14 23:50:47 +0630

    Add Bytes

--- chapter-01/bytes.py
+++ chapter-01/bytes.py
@@ -1,6 +1,7 @@
 x = [0, 10, 25, 200]
 my_bytes = bytes(x)
-print("my_bytes => ", my_bytes) #my_bytes =>  b'\x00\n\x19\xc8'
-print("type of my_bytes =>", type(my_bytes) ) #type of my_bytes => <class 'bytes'>
-#my_bytes[1] = 444 #TypeError: 'bytes' object does not support item assignment
-print("my_bytes[1]=> ", my_bytes[1]) #my_bytes[1]=>  10
+print("my_bytes => ", my_bytes)  # my_bytes =>  b'\x00\n\x19\xc8'
+# type of my_bytes => <class 'bytes'>
+print("type of my_bytes =>", type(my_bytes))
+# my_bytes[1] = 444 #TypeError: 'bytes' object does not support item assignment
+print("my_bytes[1]=> ", my_bytes[1])  # my_bytes[1]=>  10
