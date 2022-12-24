fb7cf8fc7e89a2128192812e043d6adb2fadcff8
DevIdol <devidol.mm@gmail.com>
2022-12-24 18:02:00 +0630

    add relational

---
+++ chapter-03/_01relational.py
@@ -0,0 +1,9 @@
+x = 20
+y = 15
+z = True
+
+print("x < y:", x < y) #x < y: False
+print("x + y + z:", x + y + z) #x + y + z: 36
+
+print("apple > orange:", "apple" > "orange") #apple > orange: False
+print("apple < orange:", "apple" < "orange") #apple < orange: True
