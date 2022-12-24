4a23841afe457d6e01c7d43af7588b5ba353dce5
DevIdol <devidol.mm@gmail.com>
2022-12-24 17:27:08 +0630

    add input

---
+++ chapter-01/input.py
@@ -0,0 +1,10 @@
+x = float(input("Enter x:"))
+y = float(input("Enter y:"))
+
+print("Sum:", x + y)
+print("Subtract:", x - y)
+print("Mult:", x * y)
+print("Float Division:", x / y)
+print("Integer Division:", x // y)
+print("Power:", x ** y)
+print("Remainder:", x % y)
