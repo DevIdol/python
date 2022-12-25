375838219b048c29db66da21af8a871485986b9e
DevIdol <devidol.mm@gmail.com>
2022-12-25 18:07:01 +0630

    add boolean operator

---
+++ _02boolean_operator.py
@@ -0,0 +1,21 @@
+
+
+
+# AND
+# (Left true => return right hand side)
+# (Left false => return left hand side)
+print("True and True:", True and True)  # True and True: True
+print("True and False:", True and False)  # True and False: False
+print("False and True:", False and True)  # False and True: False
+print("False and False:", False and False)  # False and False: False
+
+# OR
+# (Left true => return left hand side)
+# (Left false => return right hand side)
+print("True or True:", True or True)  # True or True: True
+print("True or False:", True or False)  # True or False: True
+print("False or True:", False or True)  # False or True: True
+print("False or False:", False or False)  # False or False: False
+
+print("not True:", not True)  # not True: False
+print("not False:", not False)  # not False: True
