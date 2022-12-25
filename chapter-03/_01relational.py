eb76a32a6bf9359ad661cd4ce271021f2be158af
DevIdol <devidol.mm@gmail.com>
2022-12-25 17:41:32 +0630

    modified realtional

--- chapter-03/_01relational.py
+++ chapter-03/_01relational.py
@@ -2,8 +2,20 @@ x = 20
 y = 15
 z = True
 
-print("x < y:", x < y) #x < y: False
-print("x + y + z:", x + y + z) #x + y + z: 36
+print("x < y:", x < y)  # x < y: False
+print("x + y + z:", x + y + z)  # x + y + z: 36
 
-print("apple > orange:", "apple" > "orange") #apple > orange: False
-print("apple < orange:", "apple" < "orange") #apple < orange: True
+print("apple > orange:", "apple" > "orange")  # apple > orange: False
+print("apple < orange:", "apple" < "orange")  # apple < orange: True
+
+print("True > 0:", True > 0)  # True > 0: True
+
+print("5 > 2 > 1:", 5 > 2 > 1)  # 5 > 2 > 1: True
+
+print("aaaa < b:", "aaa" < "b")  # aaaa < b: True
+
+lst1 = [30, 10]
+lst2 = [22, 12]
+
+print("lst1 > lst2:", lst1 > lst2) #lst1 > lst2: True
+print("lst1 < lst2", lst1 < lst2) #lst1 < lst2 False
