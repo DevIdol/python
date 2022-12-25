0c93d53bf95692c95ae12de17bc7f3b74311def6
DevIdol <devidol.mm@gmail.com>
2022-12-25 18:18:10 +0630

    add and, or semantic

---
+++ _03_and_or_semantic.py
@@ -0,0 +1,15 @@
+def getTrue():
+    print("Get True")
+    return True
+
+
+def getFalse():
+    print("Get False")
+    return False
+
+
+print("getFalse and getTrue:", getFalse() and getTrue())
+
+
+# Get False
+# getFalse and getTrue: False
