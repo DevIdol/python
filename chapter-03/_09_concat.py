8094efcf24b4bd1e5128200b8924362d6da9dc1e
DevIdol <devidol.mm@gmail.com>
2022-12-26 16:13:06 +0630

    concat

---
+++ chapter-03/_09_concat.py
@@ -0,0 +1,9 @@
+x = 'Hello' + " Python "
+
+print('x is:', x) # x is: Hello Python
+
+x *= 3
+print('x is:', x) # x is: Hello Python Hello Python Hello Python 
+
+str = "Hello" " Python"
+print("str is:", str) # str is: Hello Python
