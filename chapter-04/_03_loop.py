2996654d9dbb07878a1e56dcb681968730696105
DevIdol <devidol.mm@gmail.com>
2022-12-26 17:34:04 +0630

    loop

---
+++ chapter-04/_03_loop.py
@@ -0,0 +1,15 @@
+for i in range(3):
+    print("Hello Python", i)
+
+
+my_list = ["one", "two", "three", "four"]
+for item in my_list:
+    print("Item: ", item)
+
+my_dict = {"name": "Joe", "age": 20}
+for info in my_dict:  # print key
+    print("Info: ", info)
+
+my_set = ("python", "js", "c#")
+for type in my_set:
+    print("Type: ", type)
