8c8839acf3f53624772cfec296ae6728dca5af70
DevIdol <devidol.mm@gmail.com>
2022-12-26 16:59:41 +0630

    positive and negative

---
+++ chapter-04/_02_pos_neg.py
@@ -0,0 +1,21 @@
+# num = float(input("Enter Number: "))
+
+# if num > 0:
+#     print("Positive")
+# elif num == 0:
+#     print("Zero")
+# else:
+#     print("Negative")
+# print("Program End")
+
+
+mark = float(input("Enter mark for myanmar: "))
+
+if mark >= 80:
+    print("Pass with Distinction")
+elif mark >= 40:
+    print("Pass")
+else:
+    print("Fail")
+
+print("Program End")
