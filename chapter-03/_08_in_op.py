b537de11e431d542e1c03084da34e9c00e52d8d5
DevIdol <devidol.mm@gmail.com>
2022-12-26 16:06:49 +0630

    in operator

---
+++ chapter-03/_08_in_op.py
@@ -0,0 +1,14 @@
+str = "Hello"
+print("'Hell' in str:", "Hell" in str)  # 'Hell' in str: True
+
+lst = [10, 20, 30]
+print("10 in lst:", 10 in lst)  # 10 in lst: True
+
+my_dict = {"name": "Joe", "age": 20}
+print("name in my_dict:", "name" in my_dict)  # name in my_dict: True
+
+my_tuple = ("js", "python")
+print("python in my_tuple", "python" in my_tuple)  # python in my_tuple True
+
+my_tuple = ("Java", "JS")
+print("python not in my_tuple", "python" not in my_tuple)  # python not in my_tuple True
