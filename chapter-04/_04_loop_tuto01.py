c59845dc4fe8b36cf31a7bcb58fa9d14dedc457b
DevIdol <devidol.mm@gmail.com>
2022-12-26 18:09:36 +0630

    loop tutorial

--- chapter-04/_04_loop_tuto01.py
+++ chapter-04/_04_loop_tuto01.py
@@ -2,8 +2,17 @@ num_of_sub = int(input("Enter number of subject: "))
 marks = []
 
 pass_mark = 40
+isPass = True
 
 for i in range(num_of_sub):
     mark = int(input("Enter mark for subject: " + str(i)+" "))
     marks.append(mark)
-print("Mark: ", marks)
+    isPass = isPass and mark >= pass_mark
+print("Marks: ", marks)
+print("Total Mark: ", sum(marks))
+
+
+if isPass:
+    print("Pass the exam")
+else:
+    print("Fail the exam")
